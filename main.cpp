c7008e6452a16c8df257c415052bf2f94e2ed6f8
N.Fujita <naofuji.1220@gmail.com>
2021-11-15 17:23:14 +0900

    initial commit

--- main.cpp
+++ main.cpp
@@ -10,50 +10,29 @@
 using namespace std;
 
 
-vector<int> buf;
-vector<vector<int>> array_desc(3);  // デカルト積の配列
-// デカルト積を求める関数
-void desc(int i, const int size, const int range_start, const int range_end) {
-	buf.resize(3);
-	if (i == size) {
-		for (int i = 0; i < size; i++) {
-			array_desc[i].push_back(buf[i]);
-		}
-	}
-	else {
-		for(int j = range_start; j <= range_end; ++j) {
-			buf[i] = j;
-			desc(i+1, size, range_start, range_end);
-		}
-	}
-}
-
-
 // 気相体積を測定する関数
 void gas_volume(double d, double thresh) {
-//	ifstream ifile("rescale01.lammpstrj");  // 読み込むファイルのパスを指定
-	ifstream ifile("test.lammpstrj");  // 読み込むファイルのパスを指定
+	ifstream ifile("rescale01.lammpstrj");  // 読み込むファイルのパスを指定
+//	ifstream ifile("test.lammpstrj");  // 読み込むファイルのパスを指定
 	ofstream ofile("test.dat");  // 書き出すファイルのパスを指定
 
 	const int N = 4000;
 	int num_atoms;  // 粒子数
 	double L;  // ボックスサイズ
 	double V = pow(d, 3.0);  // セルの体積
-	int num_cell;  // セルの一次元の個数
-	int index = 0;  // dumpfileの各行の通し番号
+	int Lx, Ly, Lz;  // 各次元のセルの個数
+	int i_dump = 0;  // dumpfileの各行の通し番号
 	int i_step = 0;  // step内でのインデックス 
 	string str_dump;
 	double pos_data[N][3];  // 1stepあたりの粒子の座標データの配列→vectorへ
 
 	while (getline(ifile, str_dump)) {  // ifileを1行ずつstr_dumpに読み込む
-		if (index == 3) {
+		if (i_dump == 3) {
 			num_atoms = stoi(str_dump);  
 		}
-		if (index == 5) {
-			L = stod(split(str_dump, ' ')[1]);  
-			num_cell = (int)(L / d);  
-
-			desc(0, 3, 0, num_cell-1);  // デカルト積
+		if (i_dump == 5) {
+			L = stod(split(str_dump, ' ')[1]);  // 立方体のシミュレーションボックス
+			Lx = Ly = Lz = (int)(L / d);  // 一次元のセルの個数
 		}
 		if (split(str_dump, ' ').size() == 5) {  // 座標データの行の場合
 			i_step += 1;
@@ -63,39 +42,33 @@ void gas_volume(double d, double thresh) {
 			pos_data[i_step-1][1] = stod(split(str_dump, ' ')[3]) * L;  
 			pos_data[i_step-1][2] = stod(split(str_dump, ' ')[4]) * L;
 
-			/////座標データから密度計算//////
+			/////座標データから密度計算/////
 			if (i_step == num_atoms) {  // 1step終わったらセルごとに密度計算し、気泡体積割合を算出
+				vector<double> density(Lx * Ly * Lz, 0);  // 密度データの配列
 				int gas = 0;  // 閾値以下のセルを気泡としてカウント
-				for (int i = 0; i < array_desc[0].size(); i++) {
-					double x_cell = d * array_desc[0][i];
-					double y_cell = d * array_desc[1][i];
-					double z_cell = d * array_desc[2][i];
-					int n = 0;  // セル内の粒子数
-					for (int j = 0; j < num_atoms; j++) {
-						bool X = (x_cell <= pos_data[j][0]) && (pos_data[j][0] < x_cell + d);  
-						bool Y = (y_cell <= pos_data[j][1]) && (pos_data[j][1] < y_cell + d);
-						bool Z = (z_cell <= pos_data[j][2]) && (pos_data[j][2] < z_cell + d);
-						if (X && Y && Z) {
-							n += 1;
-						}
-					}
-					double rho = n / V;  // セルの密度
-//					ofile << rho << '\n';
-					if (rho <= thresh) {
+				for (int i = 0; i < num_atoms; i++) {
+					int mx = int(pos_data[i][0] / d);
+					int my = int(pos_data[i][1] / d);
+					int mz = int(pos_data[i][2] / d);
+					int i_density = mx + my * Lx + mz * Lx * Ly;  // 密度データの中でのインデックス
+					density[i_density] += 1.0 / V;
+				}
+				for (int i = 0; i < Lx * Ly * Lz; i++) {
+					if (density[i] <= thresh) {
 						gas += 1;
 					}
 				}
-				ofile << ((double)(gas)/(double)(pow(num_cell, 3))) << '\n';  // 気泡/全体
+				ofile << ((double)(gas)/(double)(Lx * Ly * Lz)) << '\n';  // 気泡/全体
 				i_step = 0;  // i_stepを初期化
 			}
 		}
-		index += 1;
+		i_dump += 1;
 	}
 }
 
 
 int main() {
-//	gas_volume(1.4875, 0.1);
-	gas_volume(1.7, 0.1);
+	gas_volume(1.4875, 0.1);
+//	gas_volume(1.7, 0.1);
 	return 0;
 }
